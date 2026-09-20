92cf213c500a095c764ad1d40bd47f69be8e6e0c
jtfenris <jtfenris@proton.me>
2026-09-20 12:03:32 +0200

    flatten if. comments

--- script_man.sh
+++ script_man.sh
@@ -2,27 +2,31 @@
 
 echo sanitycheck #TODO: fjernes inden aflevering
 
-# skal hentes fra en config eller lign
+#TODO: skal hentes fra en config eller lign
 file="targets.list"
 log="script_man.log"
 
-# regex fra ai
+# regex fra ai. behøver kun at forstå 
 domain_regex='^(https?://)?[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+(:[0-9]{1,5})?([/?#][^[:space:]]*)?$'
 ipv4_regex='^(https?://)?((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(:[0-9]{1,5})?([/?#][^[:space:]]*)?$'
 localhost_regex='^(https?://)?localhost(:[0-9]{1,5})?([/?#][^[:space:]]*)?$'
 
 while read -r line || [[ -n "$line" ]]; do
     timestamp=$(date +"%Y-%m-%d %H:%M:%S") #TODO: til ISO
-    if [[ "$line" =~ $domain_regex ]] ||
-       [[ "$line" =~ $ipv4_regex ]] ||
-       [[ "$line" =~ $localhost_regex ]]; then
-        curl -Isf --max-time 5 "$line" >/dev/null
-        if [ $? -eq 0 ]; then # ? er den sidste exit code. hvis den er nul, er den sidste kommando succesfuld.
-            echo "$timestamp - $line - UP" >> $log
-        else
-            echo "$timestamp - $line - DOWN" >> $log
-        fi
-    else
-        echo "$timestamp - $line - address not valid" >> script_man.log
+    
+    # checker først om target er valid, så curl ikke stopper på forkert indput. f.eks curl 1.
+    if ! ([[ "$line" =~ $domain_regex ]] ||
+          [[ "$line" =~ $ipv4_regex ]] ||
+          [[ "$line" =~ $localhost_regex ]]) ; then
+        echo "$timestamp - $line - address not valid" >> $log
+        continue
     fi
+
+    curl -Isf --max-time 5 "$line" >/dev/null # da vi ikke er interresseret i selve respond, kassere vi den.
+    if [ $? -eq 0 ]; then # ? er den sidste exit code. hvis den er nul, er den sidste kommando "succesfuld".
+        echo "$timestamp - $line - UP" >> $log
+    else
+        echo "$timestamp - $line - DOWN" >> $log
+    fi
+
 done < $file
